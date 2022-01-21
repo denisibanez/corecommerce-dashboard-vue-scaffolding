e24ad52999a039f222f6dda3995a7a6d6354dfdd
Denis Ibanez <denis.ibanez@faci.ly>
2022-01-21 16:15:46 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <span id="app1">
+    App1 is working!
+    <div>
+      <img alt="Vue logo" src="@/assets/logo.png">
+    </div>
+    <router-view></router-view>
+  </span>
+</template>
+
+<style>
+#app1 {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home">
+    <router-link to="/app1/about">App1's about page</router-link>
+  </div>
+</template>
+
